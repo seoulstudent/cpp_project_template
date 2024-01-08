fbcf3252148cbd7544e75d9992202b4266091a2f
seoulstudent <gingtom2@naver.com>
2024-01-08 19:35:57 +0900

    Added skeleton codes for TEST

---
+++ include/math/utils.hpp
@@ -0,0 +1,11 @@
+// Copyrights (c) 2024 seoulstudent, All right reserved.
+// Authors : seoulstudent
+
+#ifndef SEOULSTDENT__MATH__UTILS_HPP_
+#define SEOULSTDENT__MATH__UTILS_HPP_
+
+namespace seoulstudent::math {
+
+} // seoustudent
+
+#endif
